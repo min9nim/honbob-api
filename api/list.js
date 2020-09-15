5d1276b7f54dfd84458108d158bad157a7afb62e
keating529 <mgsong@madup.com>
2020-09-15 14:51:29 +0900

    불필요 필드 제거

--- api/list.js
+++ api/list.js
@@ -1,17 +1,17 @@
 const { getDBUrl, loadEnv, initDB } = require('../src/initialize')
 const allowCors = require('../src/utils/cors')
 const User = require('../src/models/user')
-
+const { omit } = require('ramda')
 
 const handler = async (req, res) => {
   loadEnv()
   initDB(getDBUrl())
 
   try {
-    const docs = await User.find().lean
+    const docs = await User.find().lean()
     res.json({
       status: 'ok',
-      result: docs,
+      result: omit(['createdAt', '__v'], docs),
     })
   } catch (e) {
     res.json({
