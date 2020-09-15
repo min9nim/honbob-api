e60823585e7ecbfffd73a11f72f4907e0b4a1404
keating529 <mgsong@madup.com>
2020-09-15 14:35:18 +0900

    add list api

--- api/list.js
+++ api/list.js
@@ -2,18 +2,16 @@ const { getDBUrl, loadEnv, initDB } = require('../src/initialize')
 const allowCors = require('../src/utils/cors')
 const User = require('../src/models/user')
 
+
 const handler = async (req, res) => {
   loadEnv()
   initDB(getDBUrl())
+
   try {
-    const ip =
-      req.headers['x-forwarded-for'] ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      (req.connection.socket ? req.connection.socket.remoteAddress : null)
-    await User.create({ name: req.body.name, ip })
+    const docs = await User.find().lean
     res.json({
-      status: 'ok'
+      status: 'ok',
+      result: docs,
     })
   } catch (e) {
     res.json({
