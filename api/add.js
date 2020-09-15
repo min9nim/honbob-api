f11fbfc1d8bce2bde045f6d09e0b9915f780c5c6
keating529 <mgsong@madup.com>
2020-09-15 15:07:57 +0900

    ip check

--- api/add.js
+++ api/add.js
@@ -2,6 +2,17 @@ const { getDBUrl, loadEnv, initDB } = require('../src/initialize')
 const allowCors = require('../src/utils/cors')
 const User = require('../src/models/user')
 
+function ValidateIPaddress(ipaddress) {
+  if (
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+      ipaddress,
+    )
+  ) {
+    return true
+  }
+  return false
+}
+
 const handler = async (req, res) => {
   loadEnv()
   initDB(getDBUrl())
@@ -11,9 +22,14 @@ const handler = async (req, res) => {
       req.connection.remoteAddress ||
       req.socket.remoteAddress ||
       (req.connection.socket ? req.connection.socket.remoteAddress : null)
+
+    if (!ValidateIPaddress(ip)) {
+      throw Error('Invalid IP')
+    }
+
     await User.create({ name: req.body.name, ip })
     res.json({
-      status: 'ok'
+      status: 'ok',
     })
   } catch (e) {
     res.json({
